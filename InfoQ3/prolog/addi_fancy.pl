4a19301487888cf853569cb88158098181d569cd
Jonathan Ramm <gummibaer3535@gmail.com>
2025-08-05 15:14:39 +0200

    Delete InfoQ3 directory

--- InfoQ3/prolog/addi_fancy.pl
+++
@@ -1,3 +0,0 @@
-addi(A,0,A).
-addi(A,B,Erg):- B>0,!,B1 is B-1, addi(A,B1,Zw),Erg is Zw+1.
-addi(A,B,Erg):- B<0,!,B1 is B+1, addi(A,B1,Zw),Erg is Zw-1.
--- InfoQ3/prolog/flagofppg.pl
+++
@@ -1,7 +0,0 @@
-farbe(rot).
-farbe(blau).
-farbe(gelb).
-
-kombi(A,B,C,D):-farbe(A), farbe(B), farbe(C), farbe(D),
-A \== B, A \== C, B \== C, D \== B, D\==C.
-
